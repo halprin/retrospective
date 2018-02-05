971dde6b375bb07449ec01591ce2425fdce0f78d
halprin <halprin@users.noreply.github.com>
2018-02-04 23:47:14 -0700

    Disable Django's built-in database

--- retrospective/settings.py
+++ retrospective/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_framework',
+    # 'rest_framework',
     # 'django.contrib.admin',
     # 'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -68,21 +68,7 @@ TEMPLATES = [
     },
 ]
 
-# REST_FRAMEWORK = {
-# }
-
 WSGI_APPLICATION = 'retrospective.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
