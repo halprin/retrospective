78ca0cbb792035adbef338a7481c9520c8afb7a6
halprin <halprin@users.noreply.github.com>
2018-03-08 00:06:02 -0700

    Joining a retrospective now works along with an error display when it fails

--- frontend/src/app/join-retro/join-retro.component.ts
+++ frontend/src/app/join-retro/join-retro.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse} from '@angular/common/http'
+import { RetrospectiveService } from '../retrospective.service'
 
 @Component({
   selector: 'app-join-retro',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JoinRetroComponent implements OnInit {
 
-  constructor() { }
+  errorText = '';
+
+  constructor(private router: Router, private retroService: RetrospectiveService) { }
 
   ngOnInit() {
   }
 
+  joinRetro(retroId: string, userName: string): void {
+    this.retroService.joinRetrospective(retroId, userName)
+      .subscribe(uuid => {
+        this.router.navigateByUrl('/view');
+      },
+      (error: HttpErrorResponse) => {
+        if(error.status == 404) {
+          this.displayError('The retrospective was not found.  Is the ID correct?');
+         } else {
+          this.displayError('Something bad happened.  Please contact us with the following information: [Status - ' + error.status + ', Message - ' + error.message + ']');
+         }
+      });
+  }
+
+  displayError(errorMessage: string): void {
+    this.errorText = errorMessage;
+  }
+
+  hideError(): void {
+    this.errorText = '';
+  }
 }
