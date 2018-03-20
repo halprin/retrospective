c5799c34ffad601ce002d74878ccfa2934691478
halprin <halprin@users.noreply.github.com>
2018-03-19 20:16:35 -0600

    No sessions required in the backend

--- backend/retrospective/settings.py
+++ backend/retrospective/settings.py
@@ -35,14 +35,12 @@ INSTALLED_APPS = [
     # 'django.contrib.admin',
     # 'django.contrib.auth',
     'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
     # 'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
