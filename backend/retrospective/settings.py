a17d2b660878b9234979b7d948cc580a47752fb0
halprin <halprin@users.noreply.github.com>
2018-03-19 21:22:15 -0600

    Remove password validators from Django

--- backend/retrospective/settings.py
+++ backend/retrospective/settings.py
@@ -62,25 +62,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.retrospective.wsgi.application'
 
 
-# Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-
 # Security
 SECURE_CONTENT_TYPE_NOSNIFF = True
 
