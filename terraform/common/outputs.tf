0585dcd7c8025346c2c4e67cebc69e04f6f8df00
halprin <halprin@users.noreply.github.com>
2018-04-26 23:16:58 -0600

    Create a Redis via Elasticache via Terraform

--- terraform/common/outputs.tf
+++ terraform/common/outputs.tf
@@ -5,3 +5,11 @@ output "beanstalk_application" {
 output "beanstalk_service_role" {
   value = "${data.aws_iam_role.beanstalk_service_role.name}"
 }
+
+output "message_broker_address" {
+  value = "${aws_elasticache_cluster.message_broker.cache_nodes.0.address}"
+}
+
+output "message_broker_security_group" {
+  value = "${aws_security_group.redis.id}"
+}
