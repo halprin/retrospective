048faf9314e6fe876ed3165ec2791e055e31e3f2
halprin <halprin@users.noreply.github.com>
2018-06-26 19:56:05 -0600

    Create the new database model for groups

---
+++ backend/api/modelsV2.py
@@ -0,0 +1,25 @@
+from pynamodb.attributes import UnicodeAttribute, ListAttribute, MapAttribute
+from backend.api.models import Retrospective, RetroStep, IssueAttribute
+
+
+class RetroStepV2(RetroStep):
+    ADDING_ISSUES = 'Adding Issues'
+    GROUPING = 'Grouping'
+    VOTING = 'Voting'
+    RESULTS = 'Results'
+
+
+class GroupAttribute(MapAttribute):
+    id = UnicodeAttribute()
+    title = UnicodeAttribute()
+    section = UnicodeAttribute()
+
+
+class IssueAttributeV2(IssueAttribute):
+    group = UnicodeAttribute()
+
+
+class RetrospectiveV2(Retrospective):
+    issues = ListAttribute(of=IssueAttributeV2)
+    groups = ListAttribute(of=GroupAttribute)
+    version = UnicodeAttribute()
