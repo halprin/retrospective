20bf8664b5e2ff9722d793e5df078e6084878547
halprin <halprin@users.noreply.github.com>
2018-12-25 22:40:10 -0800

    Move production deploy to support latest template

--- terraform/environments/prod/variables.tf
+++ terraform/environments/prod/variables.tf
@@ -3,30 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "beanstalk_application" {
-  type = "string"
-}
-
-variable "secret_key" {
-  type = "string"
-}
-
-variable "notification_email" {
-  type = "string"
-}
-
-variable "beanstalk_service_role" {
-  type = "string"
-}
-
 variable "base_host_name" {
-  type = "string"
-}
-
-variable "message_broker_address" {
-  type = "string"
-}
-
-variable "message_broker_security_group" {
   type = "string"
 }
