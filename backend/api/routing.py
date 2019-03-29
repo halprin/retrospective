33e94bd4703144b3af059135c0c8d430172c7fce
halprin <halprin@users.noreply.github.com>
2019-03-28 22:54:56 -0600

    Remove unnecessary remainder of Django

--- backend/api/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from backend.api.consumers import RetrospectiveConsumer
-
-websocket_urlpatterns = [
-    path('api/ws/<uuid:retro_id>', RetrospectiveConsumer),
-]
--- backend/retrospective/routing.py
+++
@@ -1,9 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-
-from backend.api import routing
-
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-    'websocket': AllowedHostsOriginValidator(URLRouter(routing.websocket_urlpatterns))
-})
